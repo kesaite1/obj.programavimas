96b186ecbdd9fe1eea5fbbc537ee133218eb15de
Emilė Kėsaitė <emakkesa@gmail.com>
2025-02-08 22:55:10 +0200

    header file

---
+++ v.pradine/my.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using std::fixed;
+using std::setprecision;
--- v.pradine/v.pradine.cpp
+++ v.pradine/v.pradine.cpp
@@ -1,16 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <iomanip>
-#include <string>
-
-using std::cout;
-using std::cin;
-using std::string;
-using std::vector;
-using std::endl;
-using std::fixed;
-using std::setprecision;
+#include "my.h"
 
 struct studentai {
     string v, pav;
