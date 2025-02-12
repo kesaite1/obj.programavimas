9e05588cfd7d0bd7e4c7d2ef603df42a09fa0493
Emilė Kėsaitė <emakkesa@gmail.com>
2025-02-12 15:39:31 +0200

    vector tipo konteineriai

---
+++ vektoriai/my.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::left;
+using std::setw;
+using std::sort;
