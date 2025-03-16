da5371d91da7dd22d6b3c74cc04524fcd337d996
Emilė Kėsaitė <emakkesa@gmail.com>
2025-03-16 13:34:15 +0200

    Pradzia

---
+++ list/functions.cpp
@@ -0,0 +1,199 @@
+#include "my.h"
+#include "code.h"
+
+static double vidurkis(studentai& A)
+{
+    double suma = 0, sk;
+    sk = A.hw.size();
+    for (int i = 0; i < sk; i++)
+    {
+        suma += A.hw[i];
+    }
+    return (sk > 0) ? suma / sk : 0;
+}
+//-------------------------------------------------------------------------------------------
+static double mediana(studentai& A)
+{
+    int sk;
+    sk = A.hw.size();
+    sort(A.hw.begin(), A.hw.end());
+    if (sk == 0) return 0;
+    if (sk % 2 == 0)  return (A.hw[sk / 2] + A.hw[(sk / 2) - 1]) / 2.0;
+    else return A.hw[sk / 2];
+}
+//-------------------------------------------------------------------------------------------
+void pazymys_mediana(studentai& A)
+{
+    A.paz_m = 0.4 * mediana(A) + 0.6 * A.egz;
+}
+//-------------------------------------------------------------------------------------------
+void pazymys_vidurkis(studentai& A)
+{
+    A.paz_vid = 0.4 * vidurkis(A) + 0.6 * A.egz;
+}
+//-------------------------------------------------------------------------------------------
+void iv1(studentai& A)
+{
+    double nd;
+    string t = "taip";
+    cout << "Iveskite studento varda ir pavarde: ";
+    cin >> A.v >> A.pav;
+    cout << "Iveskite studento egzamino pazymi: ";
+    cin >> A.egz;
+
+    while (t != "ne")
+    {
+        cout << "Iveskite studento namu darbu pazymi: ";
+        cin >> nd;
+        A.hw.push_back(nd);
+        cout << " Ar norite testi? (taip/ne): ";
+        cin >> t;
+    }
+}
+//------------------------------------------------------------------------------------------
+void iv2(studentai& A)
+{
+    double nd;
+    cout << "Iveskite studento varda ir pavarde: ";
+    cin >> A.v >> A.pav;
+    A.egz = rand() % 10 + 1;
+    for (int i = 0; i < rand() % 50 + 1; i++)
+    {
+        nd = 1.0 + (double)rand() / RAND_MAX * 9.0;
+        A.hw.push_back(nd);
+    }
+}
+//------------------------------------------------------------------------------------------
+void iv3(studentai& A, vector <string>& vardai, vector <string>& pavardes)
+{
+    double nd;
+    A.v = vardai[rand() % vardai.size()];
+    A.pav = pavardes[rand() % pavardes.size()];
+    A.egz = rand() % 10 + 1;
+    for (int i = 0; i < rand() % 50 + 1; i++)
+    {
+        nd = 1.0 + (double)rand() / RAND_MAX * 9.0;
+        A.hw.push_back(nd);
+    }
+}
+//-------------------------------------------------------------------------------------------
+double iv4(vector<studentai>& grupe, ofstream& laiko_failas)
+{
+    string choose, filename;
+    int kiek_paz;
+    double nd, glaikas, flaikas, skaitymo_laikas;
+    studentai B;
+    try {
+        cout << "Pasirinkite: generuoti nauja faila - g, ar skaityti is egzistuojancio - e: ";
+        cin >> choose;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            throw invalid_argument(" Neteisinga ivestis! Iveskite raide g arbe e.");
+        }
+
+        if (choose != "g" && choose != "e" && choose != "G" && choose != "E") {
+
+            throw out_of_range(" Neteisinga ivestis! Iveskite raide g arbe e.");
+        }
+    }
+    catch (const invalid_argument& e) { cerr << "Klaida: " << e.what() << endl; }
+    catch (const out_of_range& e) { cerr << "Klaida: " << e.what() << endl; }
+    cout << "Iveskite failo pavadinima tokiu formatu: pavadinimas.txt: ";
+    cin >> filename;
+    
+        if (choose == "g") {
+
+           glaikas = generavimo_laikas(filename, laiko_failas);
+        }
+       
+        auto skaitymo_start = high_resolution_clock::now();
+        ifstream fd(filename);
+        if (!fd)
+        {
+            cout << "Nepavyko atidaryti failo!\n";
+        }
+        string antrastes;
+        getline(fd, antrastes);
+        while (fd >> B.v >> B.pav)
+        {
+            B.hw.clear();
+            kiek_paz = 0;
+            while (fd >> nd)
+            {
+                B.hw.push_back(nd);
+                if (fd.peek() == '\n')
+                    break;
+                kiek_paz++;
+            }
+            if (kiek_paz > 0)
+            {
+                B.egz = B.hw[kiek_paz - 1];
+                B.hw.pop_back();
+            }
+            pazymys_vidurkis(B);
+            pazymys_mediana(B);
+            grupe.push_back(B);
+        }
+        auto skaitymo_end = high_resolution_clock::now();
+        skaitymo_laikas = apdorojimo_laikas(skaitymo_start, skaitymo_end);
+        laiko_failas << "Duomenu skaitymo is failo laikas: " << skaitymo_laikas << endl;
+        
+        if (choose == "g") {
+
+            flaikas = skaitymo_laikas + glaikas;
+        }
+        else flaikas = skaitymo_laikas;
+       
+        fd.close();
+        return flaikas;
+}
+//------------------------------------------------------------------------------------------------------------------
+string raide(string vardai)
+{
+    transform(vardai.begin(), vardai.end(), vardai.begin(), ::tolower);
+    return vardai;
+}
+//------------------------------------------------------------------------------------------------------------------------
+void generavimas(string failas, int dydis)
+{
+    int sk, egz;
+    double nd;
+    ofstream file(failas);
+    for (int i = 0; i < dydis; i++)
+    {
+        file << left << setw(20) << ("Vardas" + to_string(i)) << left << setw(20) << ("Pavarde" + to_string(i));
+        sk = rand() % 10 + 1;
+        for (int j = 0; j < sk; j++)
+        {
+            nd = 1.0 + (double)rand() / RAND_MAX * 9.0;
+            file << fixed << setprecision(2) << left << setw(5) << nd;
+        }
+        egz = rand() % 10 + 1;
+        file << egz << endl;
+    }
+    file.close();
+}
+//------------------------------------------------------------------------------------------------------------------------  
+double generavimo_laikas(string& filename, ofstream& laiko_failas)
+{
+    int dydis;
+    cout << "Iveskite irasu skaiciu faile: ";
+    cin >> dydis;
+	duration<double> skirtumas;
+
+        auto start = high_resolution_clock::now();
+        generavimas(filename, dydis);
+        auto end = high_resolution_clock::now();
+        skirtumas = end - start;
+    laiko_failas << "Failo generavimo laikas: " << skirtumas.count() << endl;
+    return skirtumas.count();
+}
+
+
+//------------------------------------------------------------------------------------------------------------------------
+double apdorojimo_laikas(high_resolution_clock::time_point start, high_resolution_clock::time_point end)
+{
+    duration<double> skirtumas = end - start;
+    return skirtumas.count();
+}
