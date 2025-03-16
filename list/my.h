3996da9785910b9055cc4e3439c8704833bbef3b
Emilė Kėsaitė <emakkesa@gmail.com>
2025-03-16 16:14:35 +0200

    random bibliotekos implementacija

--- list/my.h
+++ list/my.h
@@ -2,18 +2,19 @@
 #define MY_H
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <list>
 #include <iomanip>
 #include <string>
 #include <algorithm>
 #include <ctime>
 #include <chrono>
 #include <numeric>
+#include <random>
 
 using std::cout;
 using std::cin;
 using std::string;
-using std::vector;
+using std::list;
 using std::endl;
 using std::fixed;
 using std::setprecision;
@@ -30,6 +31,10 @@ using std::out_of_range;
 using std::to_string;
 using namespace std::chrono;
 using std::accumulate;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
 
 
 #endif 
--- vektoriai/my.h
+++ vektoriai/my.h
@@ -9,6 +9,7 @@
 #include <ctime>
 #include <chrono>
 #include <numeric>
+#include <random>
 
 using std::cout;
 using std::cin;
@@ -30,6 +31,10 @@ using std::out_of_range;
 using std::to_string;
 using namespace std::chrono;
 using std::accumulate;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
 
 
 #endif 
