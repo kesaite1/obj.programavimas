670ba52d09bdc4767cfa6a33525b40d412698566
Emilė Kėsaitė <emakkesa@gmail.com>
2025-03-16 13:34:15 +0200

    Pradzia

---
+++ list/code.h
@@ -0,0 +1,26 @@
+
+#ifndef CODE_H   
+#define CODE_H   
+#include "my.h"
+
+struct studentai {
+    string v, pav;
+    vector <double> hw;
+    int egz;
+    double paz_m, paz_vid;
+};
+
+static double vidurkis(studentai& A);
+static double mediana(studentai& A);
+void pazymys_mediana(studentai& A);
+void pazymys_vidurkis(studentai& A);
+void iv1(studentai& A);
+void iv2(studentai& A);
+void iv3(studentai& A, vector <string>& vardai, vector <string>& pavardes);
+double iv4(vector<studentai>& grupe, ofstream& laiko_failas);
+string raide(string vardai);
+void generavimas(string failas, int dydis);
+double generavimo_laikas(string& filename, ofstream& laiko_failas);
+double apdorojimo_laikas(high_resolution_clock::time_point start, high_resolution_clock::time_point end);
+
+#endif
